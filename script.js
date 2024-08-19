379a906c920adf532c73b396a312faeac779bfa5
rpuszkin <130287042+rpuszkin@users.noreply.github.com>
2024-08-19 17:08:50 +0200

    first letter of const test is not a huge

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function createList() {
   const ul = document.createElement("ul");
   ul.id = "list";
 
-  const Test = ["Test 1", "Test 2", "Test 3"];
+  const test = ["Test 1", "Test 2", "Test 3"];
   Test.forEach((item) => {
     const li = document.createElement("li");
     li.textContent = item;
